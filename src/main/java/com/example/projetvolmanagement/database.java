525a2b61107bcea7235d9a0740844949a902104e
Mohamed aziz belhaj hassine <101734019+medaziz2002@users.noreply.github.com>
2023-05-19 22:47:35 +0100

    first commit

---
+++ src/main/java/com/example/projetvolmanagement/database.java
@@ -0,0 +1,33 @@
+package com.example.projetvolmanagement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author MarcoMan
+ * Subscribe our Channel --> https://www.youtube.com/channel/UCPgcmw0LXToDn49akUEJBkQ
+ * Thanks for the support guys! <3
+ */
+public class database {
+
+    public static Connection connectDb(){
+
+        try{
+
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/project", "root", "");
+            return connect;
+        }catch(Exception e){e.printStackTrace();}
+        return null;
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.projetvolmanagement {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                        requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
+
+    opens com.example.projetvolmanagement to javafx.fxml;
+    exports com.example.projetvolmanagement;
+}
