b197f255756ff76641c00c7b3709bbea1c934d9c
jungleboy36 <93709766+jungleboy36@users.noreply.github.com>
2023-05-21 01:36:16 +0100

    Add files via upload

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,9 @@
 module com.example.projetvolmanagement {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                        requires org.kordamp.bootstrapfx.core;
+    requires org.kordamp.bootstrapfx.core;
     requires java.sql;
-    requires java.mail;
+    //requires java.mail;
 
     opens com.example.projetvolmanagement to javafx.fxml;
     exports com.example.projetvolmanagement;
